a76ad95d9b30e82bf63efdef5e5d6d991f123f76
RS <rspadinger@gmail.com>
2025-07-04 23:58:26 +0200

    add mappings to track token balances in WillEscrow

--- sc/contracts/WillEscrow.sol
+++ sc/contracts/WillEscrow.sol
@@ -7,10 +7,42 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 
 contract WillEscrow {
-    error Unauthorized();
+    struct TokenBalance {
+        uint256 amount;
+        address owner;
+        bool isNative;
+    }
+
+    // Mapping of (willAddress => tokenAddress => balance)
+    mapping(address => mapping(address => TokenBalance)) public tokenBalances;
+
+    // Track total native ETH per will
+    mapping(address => uint256) public nativeBalances;
+
+    // Track which contracts can call transfer functions
+    mapping(address => bool) public authorizedCallers;
+
+    // Track which wills are registered
+    mapping(address => bool) public registeredWills;
+
+    error UnauthorizedCaller();
+    error InsufficientBalance();
+    error WillNotRegistered();
+    error InvalidTransfer();
+    error NotDueYet();
+    error AlreadyExecuted();
+
+    modifier onlyAuthorized() {
+        if (!authorizedCallers[msg.sender]) revert UnauthorizedCaller();
+        _;
+    }
+
+    modifier onlyRegisteredWill() {
+        if (!registeredWills[msg.sender]) revert WillNotRegistered();
+        _;
+    }
 
     address public factory;
-    mapping(address => bool) public authorizedCallers;
 
     function setFactory(address _factory) external {
         factory = _factory;
@@ -18,24 +50,19 @@ contract WillEscrow {
     }
 
     function authorize(address caller) external {
-        if (msg.sender != factory) revert Unauthorized();
+        if (msg.sender != factory) revert UnauthorizedCaller();
         authorizedCallers[caller] = true;
     }
 
+    //@note change this => everyone can call this
     function transferETH(address payable to, uint256 amount) external {
-        if (!authorizedCallers[msg.sender]) revert Unauthorized();
-
-        //@todo check authorized  amounts
-
+        if (!authorizedCallers[msg.sender]) revert UnauthorizedCaller();
         (bool success, ) = to.call{value: amount}("");
         require(success, "ETH transfer failed");
     }
 
     function transferERC20(address token, address to, uint256 amount) external {
-        if (!authorizedCallers[msg.sender]) revert Unauthorized();
-
-        //@todo check authorized amounts
-
+        if (!authorizedCallers[msg.sender]) revert UnauthorizedCaller();
         require(IERC20(token).transfer(to, amount), "ERC20 transfer failed");
     }
 
